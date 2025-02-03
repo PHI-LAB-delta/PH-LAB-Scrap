c8c15bec09221ca16eb325b8d4b4bae1976841b3
prince raj <prince.raj@salescode.ai>
2025-02-03 13:39:55 +0530

    input prompts added

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
 const { run } = require('./src/scrappingMap');
 const { pincodeList } = require('./Data/Delhi/pincode')
 
-let fileName = "gurgoan";
-let pathName = "Data/Gurgoan";
+let fileName = "delhiOutletData.json";
+let pathName = "Data/delhi";
 
-function main() {
+const promptList = {
+    "grocery_supermarkets": [
+        "Grocery stores",
+        "Supermarkets",
+        "Convenience stores",
+        "24-hour grocery stores",
+        "Best supermarkets"
+    ],
+    "confectionery_snacks": [
+        "Candy shops",
+        "Chocolate stores",
+        "Snack stores",
+        "Where to buy chocolates",
+        "Bulk candy stores"
+    ],
+    "pet_food_supplies": [
+        "Pet stores",
+        "Where to buy pet food",
+        "Veterinary clinics with pet food supply",
+        "Best pet supply stores",
+        "Pet shops"
+    ],
+    "wholesale_bulk_purchases": [
+        "Wholesale food suppliers",
+        "Bulk snack stores",
+        "Warehouse stores",
+        "Wholesale candy suppliers",
+        "Where to buy bulk chocolates"
+    ]
+}
+
+
+async function main() {
     const localContext = {
         "extractFrom3rdWeb": false
     }
-    const type = "shops".charAt(0).toUpperCase() + "shops".slice(1);
-    console.log(type);
-    const localAreaList = pincodeList.map((val, ind) => { return val.pincode });
-    fileName = `${fileName}${type}.json`;
-    run(localAreaList, pathName, fileName, type, localContext);
+    for (let [key, prompt] of Object.entries(promptList)) {
+        for (let i = 0; i < prompt.length; i++) {
+            const localAreaList = pincodeList.map((val, ind) => { return val.pincode });
+            await run(localAreaList, pathName, fileName, prompt[i], localContext);
+        }
+    }
 }
 
 main();
