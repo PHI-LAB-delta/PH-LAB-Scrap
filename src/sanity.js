822691e560b38a3d4827a8e4b227d8aa26b4b300
prince raj <prince.raj@salescode.ai>
2025-02-08 23:52:23 +0530

    in csv

--- src/sanity.js
+++ src/sanity.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { attributeToConsider } = require('../config/sanity');
 const { parser } = require('stream-json');
 const { streamArray } = require('stream-json/streamers/StreamArray');
+const { stringify } = require('csv-stringify');
 
 function calculateWeight(stars, numberOfReviews, websites) {
     const numReviews = parseInt(numberOfReviews) || 0;
@@ -13,9 +14,9 @@ function calculateWeight(stars, numberOfReviews, websites) {
     const hasWebsite = websites && websites.length > 0 && websites[0] !== "NA";
 
     if (hasWebsite) {
-        weight *= 1.2;  // Increase weight by 20% 
+        weight *= 1.2;
     } else {
-        weight *= 1.3;  // Increase weight by 30% 
+        weight *= 1.3;
     }
 
     return Math.min(weight * 100, 100).toFixed(2) + "%";
@@ -23,13 +24,13 @@ function calculateWeight(stars, numberOfReviews, websites) {
 
 function removeAttributes(fileName, pathName) {
     const inputFilePath = path.join(pathName, fileName);
-    const outputFilePath = path.join(pathName, `filtered_${fileName}`);
+    const outputFilePath = path.join(pathName, `filtered_${fileName.replace('.json', '.csv')}`);
 
     const readStream = fs.createReadStream(inputFilePath, { encoding: 'utf8' });
     const writeStream = fs.createWriteStream(outputFilePath, { encoding: 'utf8' });
 
-    writeStream.write('[\n');
-    let isFirst = true;
+    let headersWritten = false;
+    let csvStringifier;
 
     const pipeline = readStream.pipe(parser()).pipe(streamArray());
 
@@ -59,16 +60,21 @@ function removeAttributes(fileName, pathName) {
             filteredItem["websites"]
         );
 
-        if (!isFirst) {
-            writeStream.write(',\n');
+        if (!headersWritten) {
+            const columns = Object.keys(filteredItem);
+            csvStringifier = stringify({ header: true, columns });
+
+            csvStringifier.pipe(writeStream);
+            csvStringifier.write(filteredItem);
+
+            headersWritten = true;
+        } else {
+            csvStringifier.write(filteredItem);
         }
-        writeStream.write(JSON.stringify(filteredItem, null, 2));
-        isFirst = false;
     });
 
     pipeline.on('end', () => {
-        writeStream.write('\n]\n');
-        writeStream.end();
+        csvStringifier.end();
         console.log('Filtered data written successfully to:', outputFilePath);
     });
 
