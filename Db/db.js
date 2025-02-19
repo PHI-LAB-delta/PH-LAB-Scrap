0aa1ab7de1b67a864464d5171a44fdbb2b94b015
prince raj <prince.raj@salescode.ai>
2025-02-19 12:32:13 +0530

    removed token

--- Db/db.js
+++ Db/db.js
@@ -20,7 +20,6 @@ const getOutletData = async (lob, companyPJPOutletDetails = null) => {
     dbCreds.database = `db_${lob}`;
     const tableName = "ck_outlet_details";
     console.log("creating db connection ... ");
-    console.log(dbCreds);
 
     let myPool = mysql.createPool(dbCreds).promise();
     let companyOutletDetails = [];
